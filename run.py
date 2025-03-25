2f037a90fdcd797e937043330d6cdae8ff0fa0f2
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-03-25 19:36:04 +0100

    add get categories function to get input from user

--- run.py
+++ run.py
@@ -16,4 +16,32 @@ monthlyexpenses = SHEET.worksheet('monthlyexpenses')
 
 data = monthlyexpenses.get_all_values()
 
-print(data)
+subcategories = SHEET.worksheet('subcategories')
+
+headers = subcategories.col_values(1)
+
+def get_categories():
+    """
+    ask user for a category and display sub categories
+    """
+    print('Choose a number for displaying sub categories if any : ')
+
+    row_headers = subcategories.row_values(1)
+    header_index = 0
+
+    for header in row_headers:
+        print(f'{header} : {header_index}')
+        header_index += 1
+    
+    selected_index = input("Select Category by inputing relevant Number :")
+    print(row_headers[int(selected_index)])
+
+    
+
+get_categories()
+
+
+
+
+
+
