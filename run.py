0331365fd8415b2882d80f81fc719ff0af50adb4
aremandanaveenkumar <56vkbmfw@students.codeinstitute.net>
2025-03-28 09:27:58 +0100

    add sub categories based on input from user

--- run.py
+++ run.py
@@ -26,22 +26,23 @@ def get_categories():
     """
     print('Choose a number for displaying sub categories if any : ')
 
-    row_headers = subcategories.row_values(1)
     header_index = 0
-
+    row_headers = subcategories.row_values(1)
     for header in row_headers:
-        print(f'{header} : {header_index}')
-        header_index += 1
+        if header :
+            print(f'{header} : {header_index}')
+            header_index += 1
     
-    selected_index = input("Select Category by inputing relevant Number :")
-    print(row_headers[int(selected_index)])
+    selected = input("Select Category by inputing relevant Number :")
+    selected_index = int(selected)
+    if selected_index == 0:
+        selected_index += 1
+    elif selected_index > 0:
+        selected_index += 2
+    col_headers = subcategories.col_values(selected_index)
+    print(col_headers)
 
     
-
 get_categories()
 
-
-
-
-
-
+    
